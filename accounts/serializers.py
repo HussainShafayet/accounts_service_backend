2cea12d9a6232d2652868a07ff2605be30a85e34
shafayet <shafayetsec09@gmail.com>
2025-08-18 00:43:48 +0600

    user register complete

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,21 +4,23 @@ from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, validators=[validate_password])
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ['username', 'email', 'password', 'phone_number']
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email'),
+            phone_number = validated_data['phone_number'],
             password=validated_data['password']
         )
         return user
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email')
+#class UserSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = User
+#        fields = ('id', 'username', 'email')
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,23 @@
-from rest_framework import generics, permissions
-from .serializers import RegisterSerializer, UserSerializer
-from django.contrib.auth import get_user_model
-from rest_framework.response import Response
+# accounts/views.py
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserRegistrationSerializer
 
-User = get_user_model()
+class RegisterUserAPIView(APIView):
+    """
+    API endpoint to register a new user.
+    """
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
 
-class UserView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-    def get(self, request):
-        serializer = UserSerializer(request.user)
-        return Response(serializer.data)
+#class UserView(APIView):
+#    permission_classes = [permissions.IsAuthenticated]
+#    def get(self, request):
+#        serializer = UserSerializer(request.user)
+#        return Response(serializer.data)
