b55d591cc0815399cf6569053ecca1496e8780ca
shafayet <shafayetsec09@gmail.com>
2025-08-22 20:36:20 +0600

    mobile login api update

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     #only phone_number with otp
-    #path('login/', SendOTPAPIView.as_view(), name='send-otp'),
-    #path('verify-otp/', VerifyOTPAPIView.as_view(), name='verify-otp'),
-    #path('resend-otp/', ResendOTPAPIView.as_view(), name='resend-otp'),
+    path('send-otp/', SendOTPAPIView.as_view(), name='send-otp'),
+    path('verify-otp/', VerifyOTPAPIView.as_view(), name='verify-otp'),
+    path('resend-otp/', ResendOTPAPIView.as_view(), name='resend-otp'),
 ]
